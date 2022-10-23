ae8c6d7b0ce59afd955772f69f2648630c68b176
IrohasuAndRedbull <114926323+IrohasuAndRedbull@users.noreply.github.com>
2022-10-23 22:08:41 +0900

    Update No.C

--- Homework/Class02/No.C
+++ Homework/Class02/No.C
@@ -1,1 +1,17 @@
+#include <stdio.h>
+int main() {
+	int i=0;
+	int* p;
+	void* v;
 
+	v = &i;
+	printf("%d\n", (int) v);/*iへのポインタをvに代入し，vの値を表示する*/
+
+	p = (int*)v;
+	printf("%d\n", (int) p);/*vの値をpに代入し，pの値を表示する*/
+
+	(* p)++;/*pが指す先をインクリメントする*/
+	printf("%d\n", i);/*iの値を表示する*/
+
+	return 0;
+}
